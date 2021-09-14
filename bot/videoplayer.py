74ffaebf945df9b43b948eb221b83e7fa7448dea
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-14 21:34:00 +0530

    Update videoplayer.py

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -133,21 +133,22 @@ InputStream(
                 await asyncio.sleep(2)
             await call_py.join_group_call(
                 chat_id,
-                InputStream(
-                    InputAudioStream(
-                    audio_file,
-                    AudioParameters(
-                        bitrate=48000,
-                    ),
+InputStream(
+            InputAudioStream(
+                audio_file,
+                AudioParameters(
+                    bitrate=48000,
                 ),
-                InputVideoStream(
-                    video_file,
-                    VideoParameters(
-                        width=640,
-                        height=360,
-                        frame_rate=20,
-                    ),
+            ),
+            InputVideoStream(
+                video_file,
+                VideoParameters(
+                    width=640,
+                    height=360,
+                    frame_rate=24,
                 ),
+            ),
+        ),
                 stream_type=StreamType().local_stream,
             ))
             await msg.edit("💡 **Video streaming started!**\n\n» **join to video chat on the top to watch the video.**")
